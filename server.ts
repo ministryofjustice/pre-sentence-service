a7170a01da4707a29878ecb24bb1651a4525e299
theDustRoom <paul.massey@digital.justice.gov.uk>
2021-09-15 10:24:45 +0100

    🔊 Added extra tasty banner 🔧 Updated engines
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@
  * Do appinsights first as it does some magic instrumentation work, i.e. it affects other 'require's
  * In particular, applicationinsights automatically collects bunyan logs
  */
+import fs from 'fs'
 import { initialiseAppInsights, buildAppInsightsClient } from './server/utils/azureAppInsights'
 
 initialiseAppInsights()
@@ -12,5 +13,7 @@ import app from './server/index'
 import logger from './logger'
 
 app.listen(app.get('port'), () => {
+  const data = fs.readFileSync('banner.txt', 'utf8')
+  console.log(data.toString())
   logger.info(`Server listening on port ${app.get('port')}`)
 })
