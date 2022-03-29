588de50576a503fff3d014c3a6900b317a378616
Paul Massey <theDustRoom@users.noreply.github.com>
2022-03-29 17:43:47 +0100

    🔀 🔥 Remove button from check report screen (#68)
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- integration_tests/integration/record-of-oral/checkReport.spec.ts
+++ integration_tests/integration/record-of-oral/checkReport.spec.ts
@@ -25,14 +25,5 @@ context('Oral - Check report page', () => {
     it('should include the task list', () => {
       cy.get('.moj-task-list').should('exist')
     })
-
-    it('should include the primary call to action button', () => {
-      currentPage.govukButton().contains('Sign your report').should('exist')
-    })
-
-    it('should move to correct screen upon valid form submission', () => {
-      currentPage.govukButton().contains('Sign your report').click()
-      Page.verifyOnPage(SignReport)
-    })
   })
 })
