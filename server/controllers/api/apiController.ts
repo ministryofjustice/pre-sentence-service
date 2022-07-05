9bf38d6b3a48331d8ee726ece8587d0adb1e3d1e
theDustRoom <paul.massey@digital.justice.gov.uk>
2022-07-05 17:10:47 +0100

    🧱 Changes requested by nDelius for integration
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- server/controllers/api/apiController.ts
+++ server/controllers/api/apiController.ts
@@ -6,9 +6,26 @@ import EventService from '../../services/eventService'
 export default class ApiController {
   constructor(protected readonly reportService: ReportService = null, protected readonly eventService: EventService) {}
 
+  // Support legacy nDelius report types
+  private correctReportType(reportType: string): string {
+    let correctedReportType: string
+    switch (reportType) {
+      case 'shortFormatPreSentenceReport':
+        correctedReportType = 'short-format'
+        break
+      case 'oralReport':
+        correctedReportType = 'record-of-oral'
+        break
+      default:
+        correctedReportType = ''
+    }
+    return correctedReportType.length ? correctedReportType : reportType
+  }
+
   createReport = async (req: Request, res: Response): Promise<void> => {
     try {
-      const reportDefinition = await this.reportService.getDefinitionByType(req.params.reportType)
+      const reportType = this.correctReportType(req.params.reportType)
+      const reportDefinition = await this.reportService.getDefinitionByType(reportType)
       if (!reportDefinition) {
         res.status(400).end()
         return
@@ -35,7 +52,10 @@ export default class ApiController {
         crn: req.body.crn,
         reportStatus: 'started',
       })
-      res.status(201).json(report)
+      res.status(201).json({
+        ...report,
+        urn: `uk:gov:hmpps:pre-sentence-service:report:${report.id}`,
+      })
     } catch (error) {
       res.status(error.status || 500).send(error.message)
     }
@@ -52,7 +72,8 @@ export default class ApiController {
 
   getAllReportsByType = async (req: Request, res: Response): Promise<void> => {
     try {
-      const results = await this.reportService.getAllReportsByType(req.params.reportType)
+      const reportType = this.correctReportType(req.params.reportType)
+      const results = await this.reportService.getAllReportsByType(reportType)
       res.json({
         request: req.params.reportType,
         found: results && results.length,
