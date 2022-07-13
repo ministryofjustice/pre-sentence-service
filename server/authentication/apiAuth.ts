4343cf8f6cb7546fbd03f7f1179dc3cb54165f1b
theDustRoom <paul.massey@digital.justice.gov.uk>
2022-07-13 17:15:49 +0100

    🔒️ Introduce API auth
    ➕Add jwks-rsa
    
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

---
+++ server/authentication/apiAuth.ts
@@ -0,0 +1,35 @@
+import { RequestHandler } from 'express'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { passportJwtSecret } from 'jwks-rsa'
+
+import passport from 'passport'
+
+import config from '../config'
+import logger from '../../logger'
+
+export type ApiAuthenticationMiddleware = () => RequestHandler
+
+const apiAuthenticationMiddleware: ApiAuthenticationMiddleware = () => passport.authenticate('jwt', { session: false })
+
+function initAuth(): void {
+  const opts = {
+    secretOrKeyProvider: passportJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `${config.apis.hmppsAuth.url}/.well-known/jwks.json`,
+    }),
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    ignoreExpiration: true,
+    audience: 'http://localhost:3000',
+    issuer: `${config.apis.hmppsAuth.url}/issuer`,
+    algorithms: ['RS256'],
+  }
+  const strategy = new Strategy(opts, (jwtPayload, done) => {
+    logger.info('JWT Payload:', jwtPayload)
+    done(null, false)
+  })
+  passport.use(strategy)
+}
+
+export { apiAuthenticationMiddleware, initAuth }
