910fc77f3431e746c445370857c516714445f734
theDustRoom <paul.massey@digital.justice.gov.uk>
2022-08-25 09:42:58 +0100

    ♻️ Move report started code to SharedController and corrected
    🔥 Removed startDate code from Offender Details Controller
    🔥 Removed startDate fields from Offender Details templates
    🗑️ Cleaned up
    
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- server/controllers/short-format/offenderDetailsController.ts
+++ server/controllers/short-format/offenderDetailsController.ts
@@ -1,17 +1,7 @@
 import { differenceInYears, parse } from 'date-fns'
 import BaseController from './baseController'
 
-export const pageFields: Array<string> = [
-  'name',
-  'dateOfBirth',
-  'age',
-  'crn',
-  'address',
-  'pnc',
-  'startDate-day',
-  'startDate-month',
-  'startDate-year',
-]
+export const pageFields: Array<string> = ['name', 'dateOfBirth', 'age', 'crn', 'address', 'pnc']
 
 export default class OffenderDetailsController extends BaseController {
   override templatePath = 'offender-details'
@@ -23,30 +13,13 @@ export default class OffenderDetailsController extends BaseController {
   override data: {
     dateOfBirth?: string
     age?: number
-    'startDate-day'?: string
-    'startDate-month'?: string
-    'startDate-year'?: number
   } = {}
 
-  override additionalPostAction = async () => {
-    if (this.report && this.report.status === 'NOT_STARTED') {
-      await this.reportService.updateReport({ ...this.report, status: 'STARTED' })
-    }
-  }
-
   override updateReport = async () => {
-    if (this.report && this.report.status === 'NOT_STARTED') {
-      const today = new Date()
-      const calculatedData = {
-        age: differenceInYears(today, parse(this.data.dateOfBirth, 'dd/MM/yyyy', today)),
-        'startDate-day': `0${today.getDate()}`.slice(-2),
-        'startDate-month': `0${today.getMonth() + 1}`.slice(-2),
-        'startDate-year': today.getFullYear(),
-      }
-      this.data = {
-        ...this.data,
-        ...calculatedData,
-      }
+    const today = new Date()
+    this.data = {
+      ...this.data,
+      age: differenceInYears(today, parse(this.data.dateOfBirth, 'dd/MM/yyyy', today)),
     }
   }
 }
