1af7f469b55276691e96421045be821c0f8cb0f1
theDustRoom <paul.massey@digital.justice.gov.uk>
2022-09-06 09:05:08 +0100

    🚸 Updated header link and index page
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -5,7 +5,7 @@ import asyncMiddleware from '../middleware/asyncMiddleware'
 export default function routes(router: Router): Router {
   const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
-  get('/', (req, res, next) => {
+  get('/examples', (req, res, next) => {
     res.render('pages/index')
   })
 
