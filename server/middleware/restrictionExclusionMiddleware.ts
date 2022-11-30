7e2b17a358e76446f33edb286fe0b4d10b29690b
Paul Massey <theDustRoom@users.noreply.github.com>
2022-11-30 13:46:14 +0000

    PIC-2600 correct endpoint (#161)
    * Correct Community API endpoints.
    
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>
    
    * 🔊 Add logging for debug purposes
    
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>
    
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- server/middleware/restrictionExclusionMiddleware.ts
+++ server/middleware/restrictionExclusionMiddleware.ts
@@ -5,18 +5,22 @@ import CommunityService from '../services/communityService'
 import ReportService from '../services/reportService'
 import FieldValue from '../repositories/entities/fieldValue'
 import Report from '../repositories/entities/report'
+import logger from '../../logger'
 
 export default function restrictionExclusionMiddleware(
   reportService: ReportService,
   communityService: CommunityService
 ): RequestHandler {
   return asyncMiddleware(async (req, res, next) => {
-    if (req.session && req.session.isAllowedAccess) {
+    if (req.session?.isAllowedAccess) {
       return next()
     }
+    logger.info(`Get report with id: ${req.params.reportId}`)
     const report: Report = await reportService.getReportById(req.params.reportId)
-    if (report && report.fieldValues) {
+    if (report?.fieldValues) {
+      logger.info('Report', report)
       const crnField: FieldValue = report.fieldValues.find(fieldValue => fieldValue.field.name === 'crn')
+      logger.info('CRN Field', crnField)
       try {
         const userAccess: UserAccess = await communityService.getUserAccess(crnField.value, res.locals.user.username)
         if (userAccess) {
@@ -26,10 +30,10 @@ export default function restrictionExclusionMiddleware(
       } catch (error) {
         let disallowedMessage: string
         let disallowedStack: string
-        if (error.data && error.data.userExcluded) {
+        if (error.data?.userExcluded) {
           disallowedMessage = 'User Excluded'
           disallowedStack = error.data.exclusionMessage
-        } else if (error.data && error.data.userRestricted) {
+        } else if (error.data?.userRestricted) {
           disallowedMessage = 'User Restricted'
           disallowedStack = error.data.restrictionMessage
         } else {
