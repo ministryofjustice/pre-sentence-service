23b3b9b772141ed90fc4a6c980d4d182582a3dd1
rschallapalli <98027245+rschallapalli@users.noreply.github.com>
2023-02-27 16:56:31 +0000

    🔀 PIC-2781 update tu use a local report variable (#183)

--- server/controllers/shared/autoSaveController.test.ts
+++ server/controllers/shared/autoSaveController.test.ts
@@ -10,9 +10,11 @@ describe('Route Handlers - Auto Save Controller', () => {
   let handler: AutoSaveController
   let req: Request
   let res: Response
+  const getReportByIdMock = jest.fn()
 
   beforeAll(() => {
-    mockedReportService = new ReportService()
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+    mockedReportService = { getReportById: getReportByIdMock } as any as ReportService
     handler = new AutoSaveController(mockedReportService)
   })
 
@@ -41,18 +43,20 @@ describe('Route Handlers - Auto Save Controller', () => {
 
   describe('POST', () => {
     it('should redirect to the correct view', async () => {
+      getReportByIdMock.mockReturnValueOnce({ id: 12 })
       await handler.post(req, res)
       expect(res.status).toHaveBeenCalledWith(201)
       expect(res.send).toHaveBeenCalled()
     })
 
     it('should send http 409 status when field validations fail', async () => {
+      getReportByIdMock.mockReturnValueOnce({ id: 12 })
       // eslint-disable-next-line  @typescript-eslint/no-explicit-any
       const fieldValueVersionsSpy = jest.spyOn(handler as any, 'checkFieldValueVersions')
       fieldValueVersionsSpy.mockReturnValueOnce(false)
       await handler.post(req, res)
       expect(res.status).toHaveBeenCalledWith(409)
-      expect(fieldValueVersionsSpy).toHaveBeenCalledWith(req)
+      expect(fieldValueVersionsSpy).toHaveBeenCalledWith(req, { id: 12 })
       expect(res.send).toHaveBeenCalled()
     })
   })
