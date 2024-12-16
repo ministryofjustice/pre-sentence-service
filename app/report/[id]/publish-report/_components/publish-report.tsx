3fd2a44544d954b430dbcecee23984d9a519c3a9
sameermoj <sameer.srivastava@digital.justice.gov.uk>
2024-12-16 09:47:30 +0000

    PIC-4081: Adding publish-report page

--- app/report/[id]/publish-report/_components/publish-report.tsx
+++ app/report/[id]/publish-report/_components/publish-report.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react"
-import { Caption, Heading, Panel } from 'govuk-react'
+import { Caption, Heading, ListItem, OrderedList, Panel } from 'govuk-react'
 import Link from "next/link"
 import { usePathname } from "next/navigation";
 import { ParagraphText } from "../../../_components/paragraph"
@@ -9,26 +9,62 @@ import { ParagraphText } from "../../../_components/paragraph"
 export const PublishReport = (props: { id: string }) => {
   const pathname = usePathname();
 
+  const itemListProps = {
+    items: [
+      "attitudes",
+      "responsibility",
+      "circumstances",
+      "motivations",
+      "the defendant's interpretation of the offence",
+    ],
+  };
+
   return (
     <div className="govuk-grid-column-full">
 
       <Panel title="Pre-sentence report published">
         Dylan Adam Armstrong
         <br />
-        <strong>
-          CRN:E234516
-        </strong>
+        CRN:E234516
       </Panel>
 
-      <Caption>Dylan Adam Armstrong CRN: E234516</Caption>
-      <Heading size="LARGE">Victim impact assessment</Heading>
-
-      <ParagraphText text="The victim impact assessment is used to inform sentencing options such as exclusion zones. You will be able to review this later as you are writing your sentencing proposal." />
-
-      <span className="govuk-details__summary-text govuk-!-margin-bottom-3 govuk-!-margin-top-2">
-        <Link href="#">View CPS pack</Link>
+      <span className="govuk-details govuk-!-margin-bottom-3 govuk-!-margin-top-2">
+        The report is now locked and cannot be edited.<br />
+        It is published to NDelius.
       </span>
 
+      <Heading size="MEDIUM">Next steps</Heading>
+
+      <OrderedList>
+        <ListItem>
+          <Link href="#">
+            Download the report
+          </Link>
+        </ListItem>
+
+        <ListItem>
+          Share the report with the court
+
+          <ListItem>
+            <Link href="#">
+              Upload to common platform (opens in new tab)
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link href="#">
+              Upload to court store (opens in new tab)
+            </Link>
+          </ListItem>
+
+        </ListItem>
+
+      </OrderedList>
+
+      <span className="govuk-details__summary-text govuk-!-margin-bottom-3 govuk-!-margin-top-2">
+        <Link href="#">What do you think of this service ?</Link>
+      </span>
+      (takes 30 seconds)
     </div>
   );
 };
