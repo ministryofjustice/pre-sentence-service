beabfe55f3104462e1d03847dc900e5b184357ae
sameermoj <sameer.srivastava@digital.justice.gov.uk>
2024-12-16 11:01:18 +0000

    PIC-4081: Adding publish-report page

--- app/report/[id]/publish-report/_components/publish-report.tsx
+++ app/report/[id]/publish-report/_components/publish-report.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import React from "react"
-import { Caption, Heading, ListItem, OrderedList, Panel } from 'govuk-react'
-import Link from "next/link"
+import React from "react";
+import { Heading, ListItem, OrderedList, Panel, UnorderedList } from "govuk-react";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { ParagraphText } from "../../../_components/paragraph"
 
 export const PublishReport = (props: { id: string }) => {
   const pathname = usePathname();
 
   return (
     <div className="govuk-grid-column-full">
-
       <Panel title="Pre-sentence report published">
         Dylan Adam Armstrong
         <br />
@@ -19,7 +17,8 @@ export const PublishReport = (props: { id: string }) => {
       </Panel>
 
       <span className="govuk-details govuk-!-margin-bottom-3 govuk-!-margin-top-2">
-        The report is now locked and cannot be edited.<br />
+        The report is now locked and cannot be edited.
+        <br />
         It is published to NDelius.
       </span>
 
@@ -27,32 +26,24 @@ export const PublishReport = (props: { id: string }) => {
 
       <OrderedList>
         <ListItem>
-          <Link href="#">
-            Download the report
-          </Link>
+          <Link href="#">Download the report</Link>
         </ListItem>
 
         <ListItem>
           Share the report with the court
-
-          <ListItem>
-            <Link href="#">
-              Upload to common platform (opens in new tab)
-            </Link>
-          </ListItem>
-
-          <ListItem>
-            <Link href="#">
-              Upload to court store (opens in new tab)
-            </Link>
-          </ListItem>
-
+          <UnorderedList>
+            <ListItem>
+              <Link href="#">Upload to common platform (opens in new tab)</Link>
+            </ListItem>
+            <ListItem>
+              <Link href="#">Upload to court store (opens in new tab)</Link>
+            </ListItem>
+          </UnorderedList>
         </ListItem>
-
       </OrderedList>
 
       <span className="govuk-details__summary-text govuk-!-margin-bottom-3 govuk-!-margin-top-2">
-        <Link href="#">What do you think of this service ?</Link>
+        <Link href="#">What do you think of this service?</Link>
       </span>
       (takes 30 seconds)
     </div>
