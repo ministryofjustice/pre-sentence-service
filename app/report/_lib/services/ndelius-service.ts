5fd4c6ddde8d8683d4213e34e53889b21c73167c
ArronChave3 <arron.chave@digital.justice.gov.uk>
2024-12-17 09:43:45 +0000

    Linting

--- app/report/_lib/services/ndelius-service.ts
+++ app/report/_lib/services/ndelius-service.ts
@@ -1,36 +1,7 @@
-// class BlogMD {
-//     private static instance: BlogMD;
-
-import HmppsAuthClient from "../../../../server/data/hmppsAuthClient";
-import { createRedisClient } from "../../../../server/data/redisClient";
-import TokenStore from "../../../../server/data/tokenStore";
-import PreSentenceToDeliusService from "../../../../server/services/preSentenceToDeliusService";
-
-  
-//     private constructor(directory: string) {
-//       this.directoryManager = path.join(process.cwd(), directory);
-//     }
-  
-//     static getInstance(directory: string): BlogMD {
-//       // Only create new instance if one doesn't already exist
-//       if (!this.instance) {
-//         this.instance = new BlogMD(directory);
-//       }
-//     }
-  
-//     static getInstance(directory: string): BlogMD {
-//       // Only create new instance if one doesn't already exist
-//       if (!this.instance) {
-//         this.instance = new BlogMD(directory);
-//       }
-  
-//       // Return the existing instance if one exists
-//       // or the new instance if one doesn't exist
-//       return this.instance;
-//     }
-  
-//     // Other Methods...
-//   }
+import HmppsAuthClient from '../../../../server/data/hmppsAuthClient'
+import { createRedisClient } from '../../../../server/data/redisClient'
+import TokenStore from '../../../../server/data/tokenStore'
+import PreSentenceToDeliusService from '../../../../server/services/preSentenceToDeliusService'
 
 export default class NdeliusService {
   private static instance: PreSentenceToDeliusService
