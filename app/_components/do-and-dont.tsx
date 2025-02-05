506a6c4da1a9fc430055137defa149e65d640f1e
ArronChave3 <arron.chave@digital.justice.gov.uk>
2025-02-05 13:25:35 +0000

    Updated start page content

--- app/_components/do-and-dont.tsx
+++ app/_components/do-and-dont.tsx
@@ -5,19 +5,23 @@ export const DoAndDont = (props: { dos: string[], donts: string[] }) => {
 
     const doItem = (item: string) => {
         return (<li className='flex items-center min-h-12 mt-1'>
-            <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" aria-hidden="true" width="34" height="34">
-                <path stroke-width="4" stroke-linecap="round" d="M18.4 7.8l-8.5 8.4L5.6 12" stroke="#007f3b"></path>
-            </svg>
+            <div>
+                <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" aria-hidden="true" width="34" height="34">
+                    <path stroke-width="4" stroke-linecap="round" d="M18.4 7.8l-8.5 8.4L5.6 12" stroke="#007f3b"></path>
+                </svg>
+            </div>
             {item}
         </li>)
     }
 
     const dontItem = (item: string) => {
         return (<li className='flex items-center min-h-12 mt-1'>
-            <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" width="34" height="34">
-                <path d="M17 18.5c-.4 0-.8-.1-1.1-.4l-10-10c-.6-.6-.6-1.6 0-2.1.6-.6 1.5-.6 2.1 0l10 10c.6.6.6 1.5 0 2.1-.3.3-.6.4-1 .4z" fill="#d5281b"></path>
-                <path d="M7 18.5c-.4 0-.8-.1-1.1-.4-.6-.6-.6-1.5 0-2.1l10-10c.6-.6 1.5-.6 2.1 0 .6.6.6 1.5 0 2.1l-10 10c-.3.3-.6.4-1 .4z" fill="#d5281b"></path>
-            </svg>
+            <div>
+                <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" width="34" height="34">
+                    <path d="M17 18.5c-.4 0-.8-.1-1.1-.4l-10-10c-.6-.6-.6-1.6 0-2.1.6-.6 1.5-.6 2.1 0l10 10c.6.6.6 1.5 0 2.1-.3.3-.6.4-1 .4z" fill="#d5281b"></path>
+                    <path d="M7 18.5c-.4 0-.8-.1-1.1-.4-.6-.6-.6-1.5 0-2.1l10-10c.6-.6 1.5-.6 2.1 0 .6.6.6 1.5 0 2.1l-10 10c-.3.3-.6.4-1 .4z" fill="#d5281b"></path>
+                </svg>
+            </div>
             {item}
         </li>)
     }
--- app/report/[id]/landing/_components/landing-page.tsx
+++ app/report/[id]/landing/_components/landing-page.tsx
@@ -1,59 +1,76 @@
-"use client"
+'use client'
 
-import { Heading, Table, Button } from 'govuk-react'
+import { Heading, Table, Button, ButtonArrow } from 'govuk-react'
 import Link from 'next/link'
 import React from 'react'
-import { DoAndDont } from "../../../../_components/do-and-dont"
+import { DoAndDont } from '../../../../_components/do-and-dont'
 import { IContext } from '../../../../../server/services/preSentenceToDeliusService'
 import { getRoutePath } from '../../../_lib/util/routes'
 
+export default function LandingPage(props: { id: string }) {
+  return (
+    <>
+      <Heading size="LARGE">Write a pre-sentence report (PSR)</Heading>
+      <p className="govuk-body">
+        PSRs help the judiciary determine appropriate sentences in line with their assessment of the seriousness of the
+        offence and the sentencing range set out in Sentencing Council Guidelines.
+      </p>
+      <p className="govuk-body">Use this service to write a PSR by:</p>
 
-export default function LandingPage(props:{ id: string}) {
+      <ul className="govuk-list govuk-list--bullet">
+        <li>collating your notes from the pre-sentence report interview and any other sources of information</li>
+        <li>following a guided experience through the steps to write this report</li>
+        <li>generating the final report as a PDF to share with the judiciary</li>
+      </ul>
 
-    return <>
-        <Heading size="LARGE" >Writing a pre-sentence report</Heading>
-        <p className="govuk-body">Pre-sentence reports help the judiciary determine appropriate sentences in line with their assessment of the seriousness of the offence and the sentencing range set out in Sentencing Council Guidelines.
-            Use this service to write a pre-sentence report by:</p>
+      <DoAndDont
+        dos={[
+          'provide an evidence based assessment using critical analysis',
+          'use professional curiosity to look beyond the information disclosed at interview and the CPS pack',
+          'use sentencing guidelines as a basis for your proposal',
+          'consider issues related to diversity and inclusion so that the PSR is tailored to the needs of the person',
+          'adhere to child safeguarding and domestic abuse policy expectations',
+        ]}
+        donts={[
+          'do not provide a solely descriptive account',
+          'do not accept information disclosed at face value',
+          'do not be unrealistic in your proposal or deviate from sentencing guidelines without explaining your rationale',
+          'do not disregard diversity and inclusion issues that may present',
+        ]}
+      />
 
-        <ul className='govuk-list govuk-list--bullet'>
-            <li>collating your notes from the pre-sentence report interview and any other sources of information</li>
-            <li>following a guided experience through the steps to write this report</li>
-            <li>generating the final report as a PDF to share with the judiciary</li>
-        </ul>
+      <Heading size="LARGE">Before you finish a PSR</Heading>
+      <p className="govuk-body">You can start the report at any time to enter information.</p>
+      <p className="govuk-body font-bold">
+        You will need to finish all relevant risk assessment tools before you complete and share your report.
+      </p>
+      <p className="govuk-body">You must complete both an:</p>
+      <ul className="govuk-list govuk-list--bullet">
+        <li>EPF 1 form</li>
+        <li>OASys LEVEL 1 risk assessment</li>
+      </ul>
+      <p className="govuk-body">
+        Depending on the complexity and nature of the case, you may need to complete other relevant risk assessment
+        tools.
+      </p>
+      <p className="govuk-body">
+        If requested by court you may also need to provide additional detail through a{' '}
+        <a
+          className="govuk-link"
+          href="https://equip-portal.equip.service.justice.gov.uk/CtrlWebIsapi.dll/app/documents/60BA213882A84926AD26C6A2A2A3A4A2/master"
+        >
+          dangerousness assessment.
+        </a>
+      </p>
+      <p className="govuk-body">
+        The gatekeeping process should be applied to all PSRs before they are shared with the court.
+      </p>
 
-        <DoAndDont
-            dos={
-                [
-                    'provide an evidence based assessment using critical analysis',
-                    'use professional curiosity to look beyond the information disclosed at interview and the CPS pack',
-                    'use sentencing guidelines as a basis for your proposal',
-                    'consider issues related to diversity and inclusion so that the PSR is tailored to the needs of the person',
-                    'adhere to child safeguarding and domestic abuse policy expectations'
-                ]
-            }
-            donts={
-                [
-                    'do not provide a solely descriptive account',
-                    'do not accept information disclosed at face value',
-                    'do not be unrealistic in your proposal or deviate from sentencing guidelines without explaining your rationale',
-                    'do not disregard diversity and inclusion issues that may present as this will affect the robustness of the proposal'
-                ]
-            }
-        />
-
-        <Heading size="LARGE" >Before you finish your report</Heading>
-        <p className='govuk-body'>You can start the report at any time to enter information or collate notes.</p>
-        <p className='govuk-body font-bold'>You will need to finish all relevant risk assessment tools before you complete and share your report.</p>
-        <p className='govuk-body'>You must complete both an:</p>
-        <ul className='govuk-list govuk-list--bullet'>
-            <li>EPF 1 form</li>
-            <li>OASys LEVEL 1 risk assessment</li>
-        </ul>
-        <p className='govuk-body'>Depending on the complexity and nature of the case, you may need to complete other relevant risk assessment tools such as a SARA (Spousal Assault Risk Assessment).</p>
-        <p className='govuk-body'>If requested by court you may also need to provide additional detail through dangerousness assessments to get a better understanding of risk.</p>
-
-        <Link href={getRoutePath('defendantDetails', {id: props.id})}>
-            <Button start={true}>Start</Button>
-        </Link>
+      <Link href={getRoutePath('defendantDetails', { id: props.id })}>
+        <Button icon={<ButtonArrow />} start={true}>
+          Start now
+        </Button>
+      </Link>
     </>
+  )
 }
