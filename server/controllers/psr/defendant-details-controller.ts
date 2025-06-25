371ee140b389adba081663081d5bac9bf8df2ba8
ArronChave3 <arron.chave@digital.justice.gov.uk>
2025-06-25 15:20:25 +0100

    Updated formatting

--- server/controllers/psr/defendant-details-controller.ts
+++ server/controllers/psr/defendant-details-controller.ts
@@ -1,6 +1,21 @@
+import { Response } from 'express'
+import { TemplateValues } from '../shared/sharedController'
 import BaseController from './baseController'
 
-export const pageFields: Array<string> = ['name', 'dateOfBirth', 'age', 'crn', 'address', 'pnc']
+export const pageFields: Array<string> = [
+  'name',
+  'dateOfBirth',
+  'age',
+  'crn',
+  'pnc',
+  'address-buildingName',
+  'address-number',
+  'address-streetName',
+  'address-town',
+  'address-district',
+  'address-county',
+  'address-postcode',
+]
 
 export default class DefendantDetails extends BaseController {
   override templatePath = 'psr-defendant-details'
