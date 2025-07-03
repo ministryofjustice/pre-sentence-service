8da5b15d24ce5cbd16d8da65c487b36fcb0523a7
ArronChave3 <arron.chave@digital.justice.gov.uk>
2025-07-03 10:06:25 +0100

    Removed old autosave / more unused code

--- server/routes/standardRouter.ts
+++ server/routes/standardRouter.ts
@@ -8,8 +8,6 @@ import tokenVerifier from '../data/tokenVerification'
 import setUpAuthentication from '../middleware/setUpAuthentication'
 import authorisationMiddleware from '../middleware/authorisationMiddleware'
 import populateCurrentUser from '../middleware/populateCurrentUser'
-import shortFormatRoutes from './short-format'
-import recordOfOralRoutes from './record-of-oral'
 import psrRoutes from './psr'
 import pdfRoutes from './pdf'
 
@@ -49,8 +47,6 @@ export default function standardRouter(
   })
 
   router.use(populateCurrentUser(userService))
-  router.use(shortFormatRoutes(reportService, communityService, eventService, preSentenceToDeliusService))
-  router.use(recordOfOralRoutes(reportService, communityService, eventService, preSentenceToDeliusService))
   router.use(psrRoutes(reportService, communityService, eventService, preSentenceToDeliusService))
   router.use(pdfRoutes(reportService))
 
