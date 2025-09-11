60843eae6760dec109eb0fc5f0ac2b9a36b29661
Richard Taylor <33220256+RichT900@users.noreply.github.com>
2025-09-11 16:18:35 +0100

    remove old unused code from shared controller

--- server/app.ts
+++ server/app.ts
@@ -12,8 +12,6 @@ import errorHandler from './errorHandler'
 import apiRouter from './routes/apiRouter'
 import standardRouter from './routes/standardRouter'
 import type UserService from './services/userService'
-import type CommunityService from './services/communityService'
-import type PreSentenceToDeliusService from './services/preSentenceToDeliusService'
 import GotenbergClient from './data/gotenbergClient'
 import pdfRenderer from './utils/pdfRenderer'
 import config from './config'
@@ -24,11 +22,7 @@ import setUpWebSecurity from './middleware/setUpWebSecurity'
 import setUpHealthChecks from './middleware/setUpHealthChecks'
 import setUpWebRequestParsing from './middleware/setupRequestParsing'
 
-export default function createApplication(
-  userService: UserService,
-  communityService: CommunityService,
-  preSentenceToDeliusService: PreSentenceToDeliusService
-): Application {
+export default function createApplication(userService: UserService): Application {
   const app = express()
 
   app.set('json spaces', 2)
@@ -55,7 +49,7 @@ export default function createApplication(
   )
 
   app.use('/api', apiRouter())
-  app.use('/', indexRoutes(standardRouter(userService, communityService, preSentenceToDeliusService)))
+  app.use('/', indexRoutes(standardRouter(userService)))
 
   app.use((req, res, next) => next(createError(404, 'Not found')))
   app.use(errorHandler(process.env.NODE_ENV === 'production'))
