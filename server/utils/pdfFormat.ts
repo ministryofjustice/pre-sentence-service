160acdd0a7c593a3d3093fd5f3171cb5a6147396
Richard Taylor <33220256+RichT900@users.noreply.github.com>
2026-04-23 15:01:34 +0100

    fixed a load of pdf bugs

--- server/utils/pdfFormat.ts
+++ server/utils/pdfFormat.ts
@@ -1,7 +1,7 @@
 import ReportDetails from '../repositories/entities/reportDetails'
 
 export const pdfOptions = {
-  marginTop: '0.8',
+  marginTop: '1.65',
   marginBottom: '0.7',
   marginLeft: '0.9',
   marginRight: '0.9',
@@ -14,20 +14,54 @@ interface FooterData {
 const footerDivStyle = 'display:flex; justify-content:flex-end; width:100%;'
 const headerFooterStyle =
   'font-family: Arial; font-size: 10px; width: 100%; height: 15px; padding: 10px; display: flex; align-items: center;'
+const pdfHeaderLogoStyle = 'height: 70px; width: auto; object-fit: contain;'
+const headerRowStyle =
+  'display: flex; justify-content: space-between; align-items: center; width: 100%; padding: 0 50px; box-sizing: border-box;'
 
-export function getHeader(): string {
+
+
+
+export function getHeader(armsB64: string, purpleB64: string): string {
   return `
-    <span id="qa-official" style="${headerFooterStyle}">
-      <span style="margin: 0 auto;">OFFICIAL</span>
-    </span>
+    <div style="width: 100%; box-sizing: border-box;">
+      <span id="qa-official" style="${headerFooterStyle}">
+        <span style="margin: 0 auto;">OFFICIAL</span>
+      </span>
+
+      <div style="${headerRowStyle}">
+        <img
+          src="data:image/png;base64,${armsB64}"
+          style="${pdfHeaderLogoStyle}"
+        />
+
+        <img
+          src="data:image/png;base64,${purpleB64}"
+          style="${pdfHeaderLogoStyle}"
+        />
+      </div>
+    </div>
   `
 }
 
-export function getDraftHeader(): string {
+export function getDraftHeader(armsB64: string, purpleB64: string): string {
   return `
-    <span id="qa-official" style="${headerFooterStyle}">
-      <span style="margin: 0 auto;">DRAFT VERSION - NOT FOR OFFICIAL USE</span>
-    </span>
+    <div style="width: 100%; box-sizing: border-box;">
+      <span id="qa-official" style="${headerFooterStyle}">
+        <span style="margin: 0 auto;">DRAFT VERSION - NOT FOR OFFICIAL USE</span>
+      </span>
+
+      <div style="${headerRowStyle}">
+        <img
+          src="data:image/png;base64,${armsB64}"
+          style="${pdfHeaderLogoStyle}"
+        />
+
+        <img
+          src="data:image/png;base64,${purpleB64}"
+          style="${pdfHeaderLogoStyle}"
+        />
+      </div>
+    </div>
   `
 }
 
@@ -49,7 +83,7 @@ export function getFooter(data: FooterData): string {
   return `
     <span id="qa-official-footer" style="${headerFooterStyle}">
         <div style="${footerDivStyle}">
-            <span style="text-align: right">Version: ${data.version} - Page <span class="pageNumber"></span> of <span class="totalPages"></span></stan>
+            <span style="text-align: right">Page <span class="pageNumber"></span> of <span class="totalPages"></span></stan>
         </div>
     </span>
   `
