c0e039374d1d3dd5983ac0f66c292dd9f48881a2
Peter Hurst <peter.hurst@justice.gov.uk>
2026-06-23 12:11:55 +0100

    linting, var name improvement, removed needless comments now the var names make sense

--- server/utils/validation.ts
+++ server/utils/validation.ts
@@ -3,12 +3,12 @@ import * as z from 'zod'
 export const LONG_TEXT_MAX = 20000
 export const PROPOSED_SENTENCE_MAX = 4000
 
-const NEWLINE_LIKE_CHARS = /\r\n|[\r\n\u2028\u2029]/g
-const INVISIBLE_NON_COUNTING_CHARS = /[\u00AD\u034F\u061C\u180E\u200B-\u200F\u202A-\u202E\u2060-\u2064\u2066-\u2069\uFEFF]/g
+const LINE_BREAK_CHARS = /\r\n|[\r\n\u2028\u2029]/g
+const INVISIBLE_NON_COUNTING_CHARS =
+  /[\u00AD\u034F\u061C\u180E\u200B-\u200F\u202A-\u202E\u2060-\u2064\u2066-\u2069\uFEFF]/g
 
 export const maxLengthMessage = (label: string, max: number) =>
   `${label} must be ${max.toLocaleString()} characters or fewer`
-
 
 export function normaliseForLength(value: string): string {
   return (value || '')
@@ -19,8 +19,8 @@ export function normaliseForLength(value: string): string {
     .replace(/&gt;/g, '>')
     .replace(/&#39;|&apos;/g, "'")
     .replace(/&quot;/g, '"')
-    .replace(NEWLINE_LIKE_CHARS, '') // Remove newlines, including Unicode line/paragraph separators
-    .replace(INVISIBLE_NON_COUNTING_CHARS, '') // Remove zero-width characters
+    .replace(LINE_BREAK_CHARS, '')
+    .replace(INVISIBLE_NON_COUNTING_CHARS, '')
 }
 
 export const plainTextLength = (value: string): number => normaliseForLength(value).trim().length
